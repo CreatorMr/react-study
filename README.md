4ff7d63b7afe69ecfad1b2218a1c5bf33f2a5fa9
creator <382988057@qq.com>
2018-10-31 21:55:49 +0800

    插件新增说明

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
+# 写在前头，此部分通过官网react学习。
 ### 1、初始化项目
     npm init ------生成packpage.json
-    创建dist----存放编译后的代码
-    创建src----存放开发代码
+    dist----存放编译后的代码
+    src----存放开发代码
     index.html index.js index.css 搞一套吧，放在src下面
 ### 2、使用webpack
-    安装webpack工具npm install -D webpack webpack-cli
-    安装插件  
-    npm install -D html-webpack-plugin clean-webpack-plugin webpack-dev-server css-loader webpack-merge style-loader
+    安装webpack工具
+    npm install -D webpack webpack-cli
+    npm install webpack --save-dev 安装到项目依赖中
+    安装插件 若需要使用 Webpack 开发工具，要单独安装：
+    npm install webpack-dev-server --save-dev
+    npm install html-webpack-plugin --save-dev 热更新插件，动态动态生成HTML文件
+    npm install clean-webpack-plugin  --save-dev 清除dist/build的文件
+    npm install  html-loader/file-loader 处理图片的  前面在html中使用的图片  /  在css中背景等使用图片
+    npm install -D react-hot-loader@next  热替换，增加编译效率，每次编译都进行代码的diff，只替换有所变动的地方
+    这里我们一起安装npm install -D html-webpack-plugin clean-webpack-plugin webpack-dev-server css-loader webpack-merge style-loader
 ### 3、项目启动的时候的文件
     （1）、配置文件webapck.config.js
        
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');//引入插件extract-text-webpack-plugin
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");//引入插件mini-css-extract-plugin
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
@@ -19,14 +21,40 @@ module.exports = {
                 test: /\.jsx?$/,
                 use: 'babel-loader',
                 exclude: /node_modules/
+            },{
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                {
+                    loader: 'file-loader',
+                    options: {
+                    name: '[name].[ext]',
+                    outputPath: 'images/'
+                    }
+                },
+            },{
+                test: /\.html$/,
+                use: [ {
+                  loader: 'html-loader',
+                  options: {
+                    minimize: true
+                  }
+                }],
             }
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html',
-            favicon: './favicon.ico'
+            template: './src/index.html',//根据自己的指定的模板文件来生成特定的 html 文件。这里的模板类型可以是任意你喜欢的模板，可以是 html, jade, ejs, hbs, 等等，但是要注意的是，使用自定义的模板文件时，需要提前安装对应的 loader， 否则webpack不能正确解析
+            favicon: './favicon.ico',
+            filename: 'index.html',// 默认情况下生成的 html 文件叫 index.html
+            minify: {
+                collapseWhitespace: true, //把生成的 index.html 文件的内容的没用空格去掉，减少空间
+            },
+            hash: true, //为了更好的 cache，可以在文件名后加个 hash。
         }),
         new CleanWebpackPlugin(['dist'])
+
+
+
     ]
 };
